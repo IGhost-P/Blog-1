2babe63c2ae754c127ed0cb9f0c632f24088dabc
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-03-19 16:41:55 +0900

    feat: 코드 스타일링

--- components/organisms/PostContent/index.tsx
+++ components/organisms/PostContent/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import { IPostProps } from 'types';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import * as S from './style';
 
 export function PostContent({ post }: { post: IPostProps }) {
@@ -21,15 +23,24 @@ export function PostContent({ post }: { post: IPostProps }) {
               />
             );
           },
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={dracula}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
         }}
       />
     </S.Container>
   );
 }
-
-// <Image
-//   src={`/images/posts/${post.slug}/${image.src}`}
-//   alt={image.alt}
-// width={600}
-// height={300}
-// />
--- posts/about-react.md
+++ posts/about-react.md
@@ -33,3 +33,28 @@ In this article, we'll dive into the core concepts and features NextJS has to of
 ![Create routes via your file + folder structure](Next.png)
 
 ... More content ...
+
+```javascript
+function solution(N, stages) {
+  const result = [];
+  const orderedStages = stages.sort((a, b) => a - b);
+
+  for (let i = 1; i <= N; i++) {
+    let [staging, staged] = [0, 0];
+    for (let j = 0, length = orderedStages.length; j < length; j++) {
+      if (i <= orderedStages[j]) {
+        staged += 1;
+        if (i === orderedStages[j]) staging += 1;
+      }
+    }
+    result.push([i, staging / staged]);
+  }
+
+  return result
+    .sort((a, b) => {
+      if (a[1] === b[1]) return a[0] - b[0];
+      return b[1] - a[1];
+    })
+    .map((res) => res[0]);
+}
+```
