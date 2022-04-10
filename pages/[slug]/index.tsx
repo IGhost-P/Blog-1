8e2669c88a0f6b5c59bea066f4bbf0efc623ba71
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-10 18:39:09 +0900

    feat: utterances 기능 컴포넌트화

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -1,4 +1,4 @@
-import { PostDetail } from 'components';
+import { PostDetail, Utterances } from 'components';
 import { getPostData, getPostsFiles } from 'utils/handlePosts';
 import { IPostProps } from 'types';
 
@@ -6,22 +6,7 @@ export default function PostDetailPage({ post }: { post: IPostProps }) {
   return (
     <>
       <PostDetail post={post} />
-      <section
-        ref={(elem) => {
-          if (!elem) {
-            return;
-          }
-          const scriptElem = document.createElement('script');
-          scriptElem.src = 'https://utteranc.es/client.js';
-          scriptElem.async = true;
-          scriptElem.setAttribute('repo', 'Yummy-sk/Blog');
-          scriptElem.setAttribute('issue-term', 'pathname');
-          scriptElem.setAttribute('theme', 'github-light');
-          scriptElem.setAttribute('label', 'blog-comment');
-          scriptElem.crossOrigin = 'anonymous';
-          elem.appendChild(scriptElem);
-        }}
-      />
+      <Utterances />
     </>
   );
 }
