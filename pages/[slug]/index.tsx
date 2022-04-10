43fb6ed05ecb0dc4ced3af2462d0bf27237ff04b
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-10 18:36:07 +0900

    feat: utterances 적용

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -3,7 +3,27 @@ import { getPostData, getPostsFiles } from 'utils/handlePosts';
 import { IPostProps } from 'types';
 
 export default function PostDetailPage({ post }: { post: IPostProps }) {
-  return <PostDetail post={post} />;
+  return (
+    <>
+      <PostDetail post={post} />
+      <section
+        ref={(elem) => {
+          if (!elem) {
+            return;
+          }
+          const scriptElem = document.createElement('script');
+          scriptElem.src = 'https://utteranc.es/client.js';
+          scriptElem.async = true;
+          scriptElem.setAttribute('repo', 'Yummy-sk/Blog');
+          scriptElem.setAttribute('issue-term', 'pathname');
+          scriptElem.setAttribute('theme', 'github-light');
+          scriptElem.setAttribute('label', 'blog-comment');
+          scriptElem.crossOrigin = 'anonymous';
+          elem.appendChild(scriptElem);
+        }}
+      />
+    </>
+  );
 }
 
 export function getStaticProps({ params }: { params: { slug: string } }) {
